8537274f1584f8bc78193e9e23db6c0eeac38791
BENYAMANI <ll4442556@gmail.com>
2026-07-13 01:17:31 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.CARROM_SUPABASE = {
   url: "https://fzzdijwosopjhlfklcia.supabase.co",
-  anonKey: sb_publishable_IdxvDU9MNrFONqMVW2xL4Q_WAnCkM5h
+  anonKey: "sb_publishable_IdxvDU9MNrFONqMVW2xL4Q_WAnCkM5h"
 };
